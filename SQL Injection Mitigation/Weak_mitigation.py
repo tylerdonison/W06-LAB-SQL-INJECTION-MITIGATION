d5d776694ea8668964d42bc051163bafd85e8528
tylerdonison <86095868+tylerdonison@users.noreply.github.com>
2022-02-11 19:45:55 -0700

    Update Weak_mitigation.py

--- SQL Injection Mitigation/Weak_mitigation.py
+++ SQL Injection Mitigation/Weak_mitigation.py
@@ -1,22 +1,7 @@
 def genQueryWeak(username, password):
     #provides basic mitigation through editing inputs to create desired or allowed inputs.
     invalid = ['-', ':', ';', '/', "'", ]
-    count= 0
-    length = len(username)
-    for x in range(length):
-        if x + 1 == length:
-            break
-        if username[x - count] in invalid:
-            username = username.replace(username[x - count], '')
-            length -= 1
-            count += 1
-    length = len(password)
-    count = 0
-    for x in range(length):
-        if x + 1 == length:
-            break
-        if password[x - count] in invalid:
-            password = password.replace(password[x - count], '')
-            length -= 1
-            count += 1
+    for x in invalid:
+        username = username.replace(x, '')
+        password = password.replace(x, '')
     return username, password
